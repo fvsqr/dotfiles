f3368d15412d4ad7eef7dab76d25dbb3d0cc5be3
Jan Burkl <jan@5square.de>
2024-04-03 16:08:36 +0200

    homebrew customization

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,6 +4,7 @@
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
+#
 
 # Check for Homebrew
 if test ! $(which brew)
@@ -13,7 +14,9 @@ then
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    mkdir -p ~/.homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew
+
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
